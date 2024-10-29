1dbeb3a6537081e229b722964004c20eff328042
bjKim2 <rlaxodjs3@naver.com>
2024-10-30 05:26:22 +0900

    productList update

--- src/page/AdminProductPage/component/NewItemDialog.js
+++ src/page/AdminProductPage/component/NewItemDialog.js
@@ -8,6 +8,7 @@ import {
   clearError,
   createProduct,
   editProduct,
+  getProductList
 } from "../../../features/product/productSlice";
 
 const InitialFormData = {
@@ -34,7 +35,11 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
   
 
   useEffect(() => {
-    if (success) setShowDialog(false);
+    if (success) {setShowDialog(false)
+      dispatch(getProductList());
+    };
+
+
   }, [success]);
 
   useEffect(() => {
